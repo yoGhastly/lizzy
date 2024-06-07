98269f10bf077ab004a70587f450329951e33128
yoGhastly <ve06976@innovaccion.mx>
2024-06-06 23:20:22 -0600

    feat(product-page): add detailed product view
    This commit introduces a detailed product view on the product page.
    - It includes product images, descriptions, price, color variants,and buttons for adding to cart and choosing delivery options.
    - The product image also has a hover scale transition effect.

---
+++ app/modules/products/components/product-breadcrumb.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+export const ProductBreadcrumb = ({
+  productCategory,
+}: {
+  productCategory: string;
+}) => {
+  return (
+    <div className="text-sm breadcrumbs text-muted-gray">
+      <ul>
+        <li>
+          <Link href="/catalogo/productos?category=all" className="flex gap-2">
+            <ArrowLeftIcon className="h-4" />
+            Volver al Catálogo
+          </Link>
+        </li>
+        <li>{productCategory}</li>
+      </ul>
+    </div>
+  );
+};
