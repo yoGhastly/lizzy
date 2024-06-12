45b52c547ed2edb24540a1ad8360188f7c775473
yoGhastly <ve06976@innovaccion.mx>
2024-06-11 23:03:54 -0600

    feat: change import path

--- app/modules/products/components/card.tsx
+++ app/modules/products/components/card.tsx
@@ -2,7 +2,7 @@
 import { HeartIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
-import { Product } from "../types";
+import { Product } from "../domain/Product";
 
 interface Props {
   product: Product;
